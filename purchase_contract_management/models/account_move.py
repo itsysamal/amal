23d4c1341ea90d2727174c587f4644b4c1de9f02
doaa <doaakhaled6969@gmail.com>
2020-09-24 18:54:29 +0200

    [Add]

--- purchase_contract_management/models/account_move.py
+++ purchase_contract_management/models/account_move.py
@@ -5,3 +5,8 @@ class AccountMove(models.Model):
     _inherit = 'account.move'
 
     contract_id = fields.Many2one('purchase.contract', string="Purchase Contract", readonly=True)
+
+class AccountMoveLine(models.Model):
+    _inherit = 'account.move.line'
+
+    contract_id = fields.Many2one('purchase.contract',related='move_id.contract_id', string="Purchase Contract", readonly=True,store=True)
