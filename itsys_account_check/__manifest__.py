1e0bdec9cba6687925756101705aa1040b522d49
Marwa <marwa1995393@gmail.com>
2021-03-17 11:05:55 +0200

    [add]

--- itsys_account_check/__manifest__.py
+++ itsys_account_check/__manifest__.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
+# Add company_id to Account Checks related to task RWD-19
 {
     'name': 'Account Checks',
     'version': '1.0',
@@ -10,13 +11,15 @@
     'summary': '',
     'description': "Payment through checks",
     'website': 'https://www.it-syscorp.com',
-    'depends': ['account_accountant'],
+    'depends': ['account_accountant','account'],
     'data': [
         'security/account_check_security.xml',
         'security/ir.model.access.csv',
-        'wizard/hash_to_supplier_view.xml',
+        'account_check_data.xml',
         'account_check_view.xml',
-        # 'account_check_data.xml',
+        'wizard/hash_to_supplier_view.xml',
+        'chemical_bank.xml',
+
     ],
     'installable': True,
     'auto_install': False,
--- itsys_account_check/models/__init__.py
+++ itsys_account_check/models/__init__.py
@@ -1,2 +1,1 @@
 from . import account_check
-from . import wizard
